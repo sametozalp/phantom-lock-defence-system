8e237a26003063cef096b9209260c9094d581ccf
Samet Özalp <93615071+sametozalp@users.noreply.github.com>
2026-05-19 16:28:00 +0300

    add dashboard state

---
+++ presentation/state/dashboardstate.cpp
@@ -0,0 +1,12 @@
+#include "dashboardstate.h"
+
+DashboardState::DashboardState() {}
+
+std::vector<MapObject> &DashboardState::getMapObjects() {
+    return this->mapObjects;
+}
+
+void DashboardState::onMapObjectReceived(std::vector<MapObject> &mapObjects) {
+    this->mapObjects = mapObjects;
+    emit objectsChanged();
+}
---
+++ presentation/state/dashboardstate.h
@@ -0,0 +1,25 @@
+#ifndef DASHBOARDSTATE_H
+#define DASHBOARDSTATE_H
+
+#include <QObject>
+#include "mapobject.h"
+
+class DashboardState: public QObject
+{
+    Q_OBJECT
+public:
+    DashboardState();
+
+    std::vector<MapObject> &getMapObjects();
+
+public slots:
+    void onMapObjectReceived(std::vector<MapObject> &mapObjects);
+
+signals:
+    void objectsChanged();
+
+private:
+    std::vector<MapObject> mapObjects;
+};
+
+#endif // DASHBOARDSTATE_H
